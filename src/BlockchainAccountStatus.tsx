fb33b2000e6324179c8aa0fc4b6a9f3d87732140
Vu Hoang Minh <vuhminh@gmail.com>
2019-11-28 14:33:59 +0700

    fix icon

--- src/BlockchainAccountStatus.tsx
+++ src/BlockchainAccountStatus.tsx
@@ -1,6 +1,6 @@
 import { Box, Text, ThemeContext } from 'paramount-ui';
 import React from 'react';
-import { Image, TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
 import { trimAddress } from './EthUtils';
 import { useWeb3Dialogs } from './Web3DialogsProvider';
@@ -37,13 +37,10 @@ export const BlockchainAccountStatus = (
       >
         {account && (
           <Box paddingRight={16}>
-            <Image
-              source={{
-                uri: `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='16' height='16' viewBox='0 0 16 16'%3E%3Cdefs%3E%3Ccircle id='a' cx='8' cy='8' r='8'/%3E%3C/defs%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cmask id='b' fill='%23fff'%3E%3Cuse xlink:href='%23a'/%3E%3C/mask%3E%3Cuse fill='%23D8D8D8' xlink:href='%23a'/%3E%3Cg fill-rule='nonzero' mask='url(%23b)'%3E%3Cpath fill='%23FA3A00' d='M-1.334-1.334h16.667v16.667H-1.334z'/%3E%3Cpath fill='%23018E72' d='M1.758 17.153L-4.237 1.601l15.551-5.995 5.995 15.552z'/%3E%3Cpath fill='%2318CDF2' d='M20.992 16.147L4.33 15.852 4.624-.811l16.663.295z'/%3E%3Cpath fill='%23C81477' d='M27.6 22.61l-15.773 5.382-5.382-15.774 15.773-5.381z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E%0A"`,
-                height: 16,
-                width: 16,
-              }}
-              accessibilityLabel="metamask account icon"
+            <img
+              alt="metamask account icon"
+              style={{ width: 16, height: 16 }}
+              src="https://res.cloudinary.com/wetrust-cryptounlocked/image/upload/v1574922854/meta-mask-web-3-wallet-icon.svg"
             />
           </Box>
         )}
